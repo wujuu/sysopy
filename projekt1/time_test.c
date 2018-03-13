e23d8caf809de7be412b64f1779dff56ccd3d81f
wujuu <patryk.zbigniew.wojtowicz@gmail.com>
2018-03-13 09:42:58 -0700

    qweweq

--- projekt1/time_test.c
+++ projekt1/time_test.c
@@ -10,30 +10,24 @@
 
 
 int main(int argc, char** argv){
-    //10 10 static add delete find 20 10 1999
+    //10 10 static 2000, 6, 6;
 
     int blocksSize = atoi(argv[1]);
     int charsSize = atoi(argv[2]);
     char* alloc_type = argv[3]; //static / dynamic
-    char** actions = calloc(3, sizeof(char));
-    int* indexs = calloc(3, sizeof(int));
+    int* test_atr = calloc(3, sizeof(int));
 
-    const char* dynamic = "dynamic";
-    const char* add = "add"; 
-    const char* del = "del";
-    const char* find = "find";
 
-    for(int i = 0; i<3; i++){
-        actions[i] = argv[i + 4];
-    }
-
-     for(int i = 0; i<3; i++){
-         indexs[i] = atoi(argv[i + 7]);
+     for(int i = 0; i < 3; i++){
+         indexs[i] = atoi(argv[i + 4]);
      }
      
 
     if(strcmp(alloc_type, dynamic) == 0){
+
+        //measure time
         charBlockArray* dynamicArray = makeRandCharBlockArray(blocksSize, charsSize);
+        //end measure
 
         for(int i = 0; i<3; i++){
             if(strcmp(actions[i], add) == 0){
