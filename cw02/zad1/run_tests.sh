3f2ed7c24bec69a8fe705642d761fbe43ee2bb58
wujuu <patryk.zbigniew.wojtowicz@gmail.com>
2018-03-18 03:57:06 -0700

    improved running test script

--- cw02/zad1/run_tests.sh
+++ cw02/zad1/run_tests.sh
@@ -5,33 +5,35 @@ TEST_RECORDS2=$2
 
 touch wyniki.txt
 
-echo "Running tests for file_mgmt library...\n"
+echo -e "Running tests for file_mgmt library...\n"
 
 
-for BYTES in 4 512, 4096, 8192
+for BYTES in 4 512 4096 8192
+do
     for TEST_RECORDS in $TEST_RECORDS1 $TEST_RECORDS2 
     do
-        echo "Generating 4 byte, $TEST_RECORDS record file...\n"
-
-        ./cmd_file_mgmt generate 4_bytes_data $TEST_RECORDS 4 
-
-
-        echo "System libraries 4 bytes, $TEST_RECRODS1 COPYING time stats: \n" >> wyniki.txt
-
-        time ./cmd_file_mgmt copy 4_bytes_data 4_bytes_data_copy_sys $TEST_RECORDS 4 sys >> wyniki.txt
+        echo -e "Generating $BYTES byte, $TEST_RECORDS record file...\n"
+        ./cmd_file_mgmt generate $BYTES_bytes_data $TEST_RECORDS $BYTES 
 
 
-        echo "C libraries 4 bytes, $TEST_RECRODS1 COPYING time stats: \n" >> wyniki.txt
 
-        time ./cmd_file_mgmt copy 4_bytes_data 4_bytes_data_copy_lib $TEST_RECORDS 4 lib >> wyniki.txt
+        echo -e "Copying $BYTES byte, $TEST_RECORDS record file with system libraries...\n"
+        echo -e "System libraries $BYTES bytes, $TEST_RECRODS1 COPYING time stats: \n" >> wyniki.txt
+        { time ./cmd_file_mgmt copy $BYTES_bytes_data $BYTES_bytes_data_copy_sys $TEST_RECORDS $BYTES sys ; } 2>> wyniki.txt
+
+        echo -e "Copying $BYTES byte, $TEST_RECORDS record file with C libraries...\n"
+        echo -e "C libraries $BYTES bytes, $TEST_RECRODS1 COPYING time stats: \n" >> wyniki.txt
+        { time ./cmd_file_mgmt copy $BYTES_bytes_data $BYTES_bytes_data_copy_lib $TEST_RECORDS $BYTES lib ; } 2>> wyniki.txt
 
 
-        echo "System libraries 4 bytes, $TEST_RECORDS SORTING time stats: \n" >> wyniki.txt
+        
+        echo -e "Sorting $BYTES byte, $TEST_RECORDS record file with system libraries...\n"
+        echo -e "System libraries $BYTES bytes, $TEST_RECORDS SORTING time stats: \n" >> wyniki.txt
+        { time ./cmd_file_mgmt sort $BYTES_bytes_data_copy_sys $TEST_RECORDS $BYTES sys ; } 2>> wyniki.txt
 
-        time ./cmd_file_mgmt sort 4_bytes_data_copy_sys $TEST_RECORDS 4 sys
-
-        echo "C libraries 4 bytes, $TEST_RECORDS SORTING time stats: \n" >> wyniki.txt
-
-        time ./cmd_file_mgmt sort 4_bytes_data_copy_sys $TEST_RECORDS 4 lib
+        echo -e "Sorting $BYTES byte, $TEST_RECORDS record file with system libraries...\n"
+        echo -e "C libraries $BYTES bytes, $TEST_RECORDS SORTING time stats: \n" >> wyniki.txt
+        { time ./cmd_file_mgmt sort $BYTES_bytes_data_copy_sys $TEST_RECORDS $BYTES lib ; } 2>> wyniki.txt
     done
+done
 
