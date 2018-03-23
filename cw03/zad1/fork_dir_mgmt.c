6adc4b8b66144cd7253d4bba8fe7765bf4f2d751
wujuu <patryk.zbigniew.wojtowicz@gmail.com>
2018-03-23 22:43:42 +0100

    1 and 2 zadanie done

---
+++ cw03/zad1/fork_dir_mgmt.c
@@ -0,0 +1,184 @@
+#include<stdio.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <time.h>
+#include <dirent.h>
+#include <string.h>
+
+void err_sys(const char *x) 
+{ 
+    perror(x); 
+    exit(1); 
+}
+
+
+
+void display_file_stats(struct stat file_stat){
+    printf("File size in bytes: %li \n", file_stat.st_size);
+    printf("File permissions: ");
+
+    if((file_stat.st_mode & S_IRUSR) > 0) printf("r");
+    else printf("-");
+
+    if((file_stat.st_mode & S_IWUSR) > 0) printf("w");
+    else printf("-");
+
+    if((file_stat.st_mode & S_IXUSR) > 0) printf("x");
+    else printf("-");
+
+    if((file_stat.st_mode & S_IRGRP) > 0) printf("r");
+    else printf("-");
+
+    if((file_stat.st_mode & S_IWGRP) > 0) printf("w");
+    else printf("-");
+
+    if((file_stat.st_mode & S_IXGRP) > 0) printf("x");
+    else printf("-");
+
+    if((file_stat.st_mode & S_IROTH) > 0) printf("r");
+    else printf("-");
+
+    if((file_stat.st_mode & S_IWOTH) > 0) printf("w");
+    else printf("-");
+
+    if((file_stat.st_mode & S_IXOTH) > 0) printf("x\n");
+    else printf("-\n");
+
+    printf("Last modified: %s \n", ctime(&file_stat.st_mtime));
+
+}
+
+void explore_dir(const char *dir_name, int comp_mode, time_t comp_date){
+
+    DIR *dir = opendir(dir_name);
+
+    if(dir == NULL){
+        err_sys("Open dir failed!");
+    }
+
+    struct dirent *file = readdir(dir);
+
+    while(file != NULL){
+        char abs_path[1024];
+        strcpy(abs_path, dir_name);
+        strcat(abs_path, "/");
+        strcat(abs_path, file -> d_name);
+
+        if(strcmp(file -> d_name, ".") != 0 && strcmp(file -> d_name, "..") != 0){
+            
+            struct stat file_stat; 
+
+            lstat(abs_path, &file_stat);
+
+            if(S_ISDIR(file_stat.st_mode)){
+                
+                pid_t pid;
+                
+                if((pid = fork()) < 0){
+                    err_sys("Fork failed!");
+                }
+
+                else if(pid == 0){
+                    explore_dir(abs_path, comp_mode, comp_date);
+                    exit(0);
+                }else{
+                    wait(NULL);
+                }
+                
+            }
+            else if (S_ISREG(file_stat.st_mode)){
+                if( (comp_mode == -1 && comp_date < file_stat.st_mtime) ||
+                   (comp_mode == 0 && comp_date == file_stat.st_mtime) ||
+                   (comp_mode == 1 && comp_date > file_stat.st_mtime)){
+
+                    printf("Absolute path: %s\n", abs_path);
+                    display_file_stats(file_stat);
+                }
+            }
+        }
+
+        file = readdir(dir);
+    }
+
+    closedir(dir);
+}
+
+
+
+int main(int argc, char **argv){
+// ./a.out /home/patryk/Desktop/sysopy -1 2018 3 18 18 50 50
+
+    struct tm tm_time;
+    time_t raw_time; 
+
+    if(argc != 9){
+        err_sys("Wrong number of agruments!");
+    }
+
+    int last_index = 0;
+
+    while(argv[1][last_index] != '\0') last_index++;
+
+    if(argv[1][last_index - 1] == '/') argv[1][last_index - 1] = '\0';
+
+    char* dir_name = argv[1];
+
+
+    if(argv[1][0] != '/'){
+        char cwd[1024];
+        if(getcwd(cwd, sizeof(cwd)) == NULL){
+            err_sys("Coulnd't get cwd!");
+        };
+        strcat(cwd, "/");
+        strcat(cwd, dir_name);
+        dir_name = cwd;
+    }
+
+
+
+
+
+    if(atoi(argv[2]) < -1 || atoi(argv[2]) > 1){
+        err_sys("Wrong comparison mode!");
+    }
+
+    if(atoi(argv[3]) < 1900){
+        err_sys("Wrong year!");
+    }
+
+    if(atoi(argv[4]) < 1 || atoi(argv[4]) > 12){
+        err_sys("Wrong month!");
+    }
+
+    if(atoi(argv[5]) < 1 || atoi(argv[5]) > 31){
+        err_sys("Wrong day!");
+    }
+
+    if(atoi(argv[6]) < 0 || atoi(argv[6]) > 23){
+        err_sys("Wrong hour!");
+    }
+
+    if(atoi(argv[7]) < 0 || atoi(argv[7]) > 59){
+        err_sys("Wrong minute!");
+    }
+
+    if(atoi(argv[8]) < 0 || atoi(argv[8]) > 59){
+        err_sys("Wrong second!");
+    }
+
+    tm_time.tm_year = atoi(argv[3]) - 1900;
+    tm_time.tm_mon = atoi(argv[4]) - 1;
+    tm_time.tm_mday = atoi(argv[5]);
+    tm_time.tm_hour = atoi(argv[6]);
+    tm_time.tm_min = atoi(argv[7]);
+    tm_time.tm_sec = atoi(argv[8]);
+    tm_time.tm_isdst = 0;
+
+    raw_time = mktime(&tm_time);
+
+    explore_dir(dir_name, atoi(argv[2]), raw_time);
+
+    return 0;
+}
---
+++ cw03/zad2/fork_test.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <string.h>
+
+int MAX_ARG_SIZE = 20;
+
+
+void display_command(char ***commands, int index){
+
+    int j = 0;
+    
+    for(j = 0; j < MAX_ARG_SIZE && commands[index][j] != NULL; j++)
+        printf("%s ", commands[index][j]);
+
+    printf("\n");
+}
+
+
+char ***parse_commands(char *commands_file_path, int *commands_amount){
+
+    struct stat commands_stat;
+    int commands_size, i, j;
+    char *r_buff;
+    FILE *commands_ptr;
+    char * full_command, *sub_command;
+    char *** commands;
+
+
+    *commands_amount = 0;
+
+
+    //READING FILE STATS
+    if(stat(commands_file_path, &commands_stat) == -1){
+        perror("Failed to get file stats");
+        exit(1);
+    }
+
+    commands_size = commands_stat.st_size;
+    
+
+    //READING FILE
+    r_buff = malloc(commands_size);
+
+    commands_ptr = fopen(commands_file_path, "r");
+
+    if(commands_ptr == NULL){
+        perror("Open commands file failed!");
+        exit(1);
+    }
+
+    fread(r_buff, sizeof(char), commands_size, commands_ptr);
+
+    fclose(commands_ptr);
+
+
+
+    //COUNTING AMOUNT OF COMMANDS IN THE FILE
+    for(i = 0; i < commands_size; i++){
+        if(r_buff[i] == '\n'){
+            (*commands_amount)++;
+        }
+    }
+
+    //ALLOCATING ENOUGH SPACE FOR COMMAND ARRAY
+    commands = malloc(((*commands_amount) + 1) * sizeof(char**));
+
+
+    //ALLOCATING AND INITIALIZING COMMAND ARRAY
+    for(i = 0; i < (*commands_amount) + 1; i++){
+
+        commands[i] = malloc(MAX_ARG_SIZE * sizeof(char*));
+
+        for(j = 0; j < MAX_ARG_SIZE; j++){
+            commands[i][j] = NULL;
+        }
+    }
+
+    
+
+    //SPLITTING RAW FILE DATA INTO COMMAND ARRAY
+    i = 0;
+    while((full_command = strsep(&r_buff, "\n")) ){
+        j = 0;
+        while(sub_command = strsep(&full_command, " ")){
+            commands[i][j] = sub_command;
+            j++;
+        }
+        i++;
+    }
+
+
+
+    return commands;
+}
+
+
+int main(int argc, char **argv){
+
+
+    int commands_amount;
+
+    //PARSING COMMANDS INTO NICE COMMAND ARRAY
+    char ***commands = parse_commands(argv[1], &commands_amount);
+
+
+    //MAIN INTERPRETER LOOP
+    for(int i = 0; i < commands_amount; i++){
+        pid_t pid;
+
+        //FORKING
+        if((pid = vfork()) < 0){
+            perror("Fork failed!");
+        }
+        else if(pid == 0){  //IF CHILD, EXECUTE COMMAND
+
+            printf("Command: ");
+            display_command(commands, i);
+
+            if (execvp(commands[i][0], commands[i])){
+                perror("Failed to execute command!");
+                exit(1);
+            }
+        }
+        else if(pid > 0){   //IF PARENT, WAIT FOR CHILD TO EXECUTE COMMAND
+            int statloc;
+
+            wait(&statloc);
+
+            if(statloc != 0){
+                exit(0);
+            }
+
+            printf("\n");     
+        }
+    }
+    exit(0);
+}
